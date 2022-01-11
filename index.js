605f39d9abbff5d4a4b551dd5c23f6feb3c846b6
Carlos Mercado <cjmbarca11@gmail.com>
2022-01-11 13:03:23 -0800

    Added AWS Rekognition features

--- index.js
+++ index.js
@@ -48,15 +48,81 @@ function uploadFile(imageUrl) {
         }
         console.log(`File uploaded successfully. ${data.Location}`);
       });
+      getGender(filename);
     })
     .catch((err) => console.error(err));
+}
+
+function getGender(filename) {
+  const config = new AWS.Config({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    region: process.env.AWS_REGION
+  })
+  const client = new AWS.Rekognition();
+  const params = {
+    Image: {
+      S3Object: {
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Name: filename
+      },
+    },
+    Attributes: ['ALL']
+  }
+  client.detectFaces(params, function(err, response) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+    } else {
+      console.log(`Detected faces for: ${filename}`)
+      response.FaceDetails.forEach(data => {
+        let low  = data.AgeRange.Low
+        let high = data.AgeRange.High
+        console.log(`The detected face is between: ${low} and ${high} years old`)
+        console.log("All other attributes:")
+        console.log(`  BoundingBox.Width:      ${data.BoundingBox.Width}`)
+        console.log(`  BoundingBox.Height:     ${data.BoundingBox.Height}`)
+        console.log(`  BoundingBox.Left:       ${data.BoundingBox.Left}`)
+        console.log(`  BoundingBox.Top:        ${data.BoundingBox.Top}`)
+        console.log(`  Age.Range.Low:          ${data.AgeRange.Low}`)
+        console.log(`  Age.Range.High:         ${data.AgeRange.High}`)
+        console.log(`  Smile.Value:            ${data.Smile.Value}`)
+        console.log(`  Smile.Confidence:       ${data.Smile.Confidence}`)
+        console.log(`  Eyeglasses.Value:       ${data.Eyeglasses.Value}`)
+        console.log(`  Eyeglasses.Confidence:  ${data.Eyeglasses.Confidence}`)
+        console.log(`  Sunglasses.Value:       ${data.Sunglasses.Value}`)
+        console.log(`  Sunglasses.Confidence:  ${data.Sunglasses.Confidence}`)
+        console.log(`  Gender.Value:           ${data.Gender.Value}`)
+        console.log(`  Gender.Confidence:      ${data.Gender.Confidence}`)
+        console.log(`  Beard.Value:            ${data.Beard.Value}`)
+        console.log(`  Beard.Confidence:       ${data.Beard.Confidence}`)
+        console.log(`  Mustache.Value:         ${data.Mustache.Value}`)
+        console.log(`  Mustache.Confidence:    ${data.Mustache.Confidence}`)
+        console.log(`  EyesOpen.Value:         ${data.EyesOpen.Value}`)
+        console.log(`  EyesOpen.Confidence:    ${data.EyesOpen.Confidence}`)
+        console.log(`  MouthOpen.Value:        ${data.MouthOpen.Value}`)
+        console.log(`  MouthOpen.Confidence:   ${data.MouthOpen.Confidence}`)
+        console.log(`  Emotions[0].Type:       ${data.Emotions[0].Type}`)
+        console.log(`  Emotions[0].Confidence: ${data.Emotions[0].Confidence}`)
+        console.log(`  Landmarks[0].Type:      ${data.Landmarks[0].Type}`)
+        console.log(`  Landmarks[0].X:         ${data.Landmarks[0].X}`)
+        console.log(`  Landmarks[0].Y:         ${data.Landmarks[0].Y}`)
+        console.log(`  Pose.Roll:              ${data.Pose.Roll}`)
+        console.log(`  Pose.Yaw:               ${data.Pose.Yaw}`)
+        console.log(`  Pose.Pitch:             ${data.Pose.Pitch}`)
+        console.log(`  Quality.Brightness:     ${data.Quality.Brightness}`)
+        console.log(`  Quality.Sharpness:      ${data.Quality.Sharpness}`)
+        console.log(`  Confidence:             ${data.Confidence}`)
+        console.log("------------")
+        console.log("")
+      })
+    }
+  });
 }
 
 app.get('/getImages', (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'application/json',
   });
-  console.log('Images : ', JSON.stringify(images));
 
   res.end(JSON.stringify(images));
 });
@@ -67,7 +133,6 @@ app.post('/findImages', function(req, res) {
     tbm: "isch",
     ijn: "0"
     }, (result) => {
-      console.log(result);
       uploadFile(result.images_results[0].thumbnail);
       images.push(result);
   });
